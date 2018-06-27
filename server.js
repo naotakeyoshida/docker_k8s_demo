feccc5c1c9e78ab87e44294c93582e9a06d1fe1c
naotakeyoshida <naotake.yoshida@emc.com>
2018-06-27 19:54:21 +0900

    v0.2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var handleRequest = function(request, response) {
   var os = require('os');
   var hostname = os.hostname();
   response.writeHead(200);
-  response.end("<h1>IT works!!  Host ID = " + hostname + "</h1>\n");
+  response.end("<h1>It works!!  Host ID = " + hostname + "</h1>\n");
 }
 
 var www = http.createServer(handleRequest);
