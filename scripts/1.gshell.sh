25e68f66a77e81c6c72c42e7398f2c86ded12bdf
naotakeyoshida <naotake.yoshida@emc.com>
2018-08-11 20:31:38 +0900

    v0.4

--- scripts/1.gshell.sh
+++ scripts/1.gshell.sh
@@ -3,4 +3,4 @@ echo ""
 echo "==== Start gcloud SDK installation ===="
 sudo curl https://sdk.cloud.google.com | bash
 echo "========== gcloud SDK version ========="
-gcloud --version
+/home/vagrant/google-cloud-sdk/bin/gcloud --version
