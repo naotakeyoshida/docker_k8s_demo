f5da705f1c17ec91040d668c5115e9b9524a4ec5
naotakeyoshida <naotake.yoshida@emc.com>
2018-08-21 17:16:35 +0900

    v0.4

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -16,21 +16,6 @@ echo "============================="
 echo `git --version`
 echo "============================="
 
-## Clone Repo
-cd /home/vagrant
-git clone https://github.com/naotakeyoshida/pws-handson1.git
-git clone https://github.com/naotakeyoshida/pws-handson2.git
-git clone https://github.com/naotakeyoshida/pws-handson3.git
-git clone https://github.com/naotakeyoshida/pws-handson4.git
-
-## Install CF CLI
-curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
-sudo mv cf /usr/local/bin
-echo "========================================="
-echo "cf cli was installed!"
-echo "API endpoint: https://api.run.pivotal.io"
-echo "========================================="
-
 ## Install expect
 yum install -y expect
 echo "expect installed!"
